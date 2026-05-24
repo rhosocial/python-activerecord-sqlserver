398aef6268574ece350a622f175f35e7d998f33e
zhao vistart <vistart@rho.social>
2026-05-24 19:22:47 +0800

    test(query): add bridge for unicode/emoji/injection round-trip tests

---
+++ tests/rhosocial/activerecord_sqlserver_test/feature/query/test_logging_data_summarization.py
@@ -0,0 +1,8 @@
+# tests/rhosocial/activerecord_sqlserver_test/feature/query/test_logging_data_summarization.py
+"""
+Bridge file for logging data summarization tests from the testsuite.
+
+This file imports the generic tests from the testsuite package and makes them
+discoverable by pytest in this project's test run.
+"""
+from rhosocial.activerecord.testsuite.feature.query.test_logging_data_summarization import *
